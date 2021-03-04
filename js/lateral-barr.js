729752bff9c6b7091037f0887b0dfcbcc814c97f
CarlosJCdev <ing.jcarlosmg@gmail.com>
2021-03-03 18:59:43 -0600

    git-blog se agrego la estrucutra HTML con modulos

---
+++ js/lateral-barr.js
@@ -0,0 +1,58 @@
+class LateralBarr extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+        <header class="Design">
+        <a href="index.html">
+            <span class="git"></span>
+        </a>
+    </header>
+
+    <header class="Programming">
+        <a href="git-blog.html">
+            <span class="development"></span>
+        </a>
+    </header>
+
+    <header class="Github">
+        <a href="react-blog.html">
+            <span class="github"></span>
+        </a>
+    </header>
+
+    <header class="HTML">
+        <a href="angular-blog.html">
+            <span class="html"></span>
+        </a>
+    </header>
+
+    <header class="CSS">
+        <a href="javascript-blog.html">
+            <span class="css"></span>
+        </a>
+    </header>
+
+    <header class="Angular">
+        <a href="development.html">
+            <span class="angular"></span>
+        </a>
+    </header>
+
+    <header class="React">
+        <a href="design.html">
+            <span class="react"></span>
+        </a>
+    </header>
+
+    <header class="Test">
+        <span class="test"></span>
+    </header>
+
+    <header class="Node">
+        <span class="node"></span>
+    </header>`;
+    }
+}
+customElements.define('lateral-barr', LateralBarr);
