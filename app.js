8d3753ea400167a0786902eb2bd4d2a6144f1f48
CarlosJCdev <ing.jcarlosmg@gmail.com>
2021-03-07 16:11:45 -0600

    Cambio de directorios con la nueva estructura dentro de public

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.end('working');
-});
+
+app.use(express.static('public/'));
 app.listen(3000, () => {
     console.log('Servidor ejecutandose.')
 });
